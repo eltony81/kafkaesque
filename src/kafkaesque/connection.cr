a4f7a38f2206168562794a6aaba3d3b45d30458f
tony <antonio.difluri@gmail.com>
2026-06-02 22:04:13 +0200

    Fix multithread socket serialization using complete request-response transaction mutex

--- src/kafkaesque/connection.cr
+++ src/kafkaesque/connection.cr
@@ -7,7 +7,7 @@ module Kafkaesque
     getter port : Int32
     getter use_ssl : Bool
     @socket : TCPSocket | OpenSSL::SSL::Socket::Client
-    @write_channel : Channel(Tuple(Bytes, Channel(Nil)))
+    @mutex = Mutex.new
 
     def initialize(@host : String, @port : Int32, @use_ssl : Bool = false, context : OpenSSL::SSL::Context::Client? = nil)
       tcp = TCPSocket.new(@host, @port)
@@ -18,46 +18,28 @@ module Kafkaesque
       else
         @socket = tcp
       end
-
-      @write_channel = Channel(Tuple(Bytes, Channel(Nil))).new(128)
-      spawn_writer_loop
-    end
-
-    private def spawn_writer_loop
-      spawn do
-        loop do
-          break if @socket.closed?
-          begin
-            bytes, done_channel = @write_channel.receive
-            size = bytes.size.to_i32
-            @socket.write_bytes(size, IO::ByteFormat::BigEndian)
-            @socket.write(bytes)
-            @socket.flush
-            done_channel.send(nil)
-          rescue ex : Channel::ClosedError | IO::Error
-            break
-          rescue ex
-            # log or handle other errors
-            break
-          end
-        end
-      end
     end
 
     def send_request(bytes : Bytes)
-      done_channel = Channel(Nil).new(1)
-      @write_channel.send({bytes, done_channel})
-      done_channel.receive
+      @mutex.lock
+      size = bytes.size.to_i32
+      @socket.write_bytes(size, IO::ByteFormat::BigEndian)
+      @socket.write(bytes)
+      @socket.flush
+    rescue ex
+      @mutex.unlock rescue nil
+      raise ex
     end
 
     def read_response : IO::Memory
-      # Reads message size (4-byte Int32)
       size = @socket.read_bytes(Int32, IO::ByteFormat::BigEndian)
       raise "Invalid response size: #{size}" if size <= 0
 
       buf = Bytes.new(size)
       @socket.read_fully(buf)
       IO::Memory.new(buf)
+    ensure
+      @mutex.unlock rescue nil
     end
 
     def closed? : Bool
@@ -66,7 +48,6 @@ module Kafkaesque
 
     def close
       @socket.close
-      @write_channel.close
     end
   end
 end
